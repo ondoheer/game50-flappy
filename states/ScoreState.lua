aa3988664cfe522bd0765bf4e8736683b62aa413
ondoheer <ondoheer@gmail.com>
2020-02-06 23:08:11 -0500

    Achivements absed on score

--- states/ScoreState.lua
+++ states/ScoreState.lua
@@ -10,12 +10,22 @@
 
 ScoreState = Class{__includes = BaseState}
 
+-- Trophies images
+
+
+local achievements = {
+    ["star"] = love.graphics.newImage("star.png"),
+    ["medal"] = love.graphics.newImage("medal.png"),
+    ["trophy"] = love.graphics.newImage("cup.png")
+}
+
 --[[
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
 ]]
 function ScoreState:enter(params)
     self.score = params.score
+    self.award = nil
 end
 
 function ScoreState:update(dt)
@@ -32,6 +42,17 @@ function ScoreState:render()
 
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
-
+   
+    if self.score < 5 and self.score > 0 then
+        self.award = achievements["star"]
+    elseif self.score < 10 and self.score > 5 then
+        self.award = achievements["medal"]
+    elseif self.score > 10 then
+        self.award = achievements["trophy"]
+    end
+    
+    if self.award then
+        love.graphics.draw(self.award, 50,100)
+    end
     love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
 end
