801931edfb55c84067611cf3f131dae9f575ecbe
ondoheer <ondoheer@gmail.com>
2020-02-06 22:30:57 -0500

    Random gap width between 90 and 130

--- PipePair.lua
+++ PipePair.lua
@@ -10,8 +10,8 @@
 
 PipePair = Class{}
 
--- size of the gap between pipes
-local GAP_HEIGHT = math.random( 90,130 )
+
+
 
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
@@ -23,10 +23,14 @@ function PipePair:init(y)
     -- y value is for the topmost pipe; gap is a vertical shift of the second lower pipe
     self.y = y
 
+    -- size of the gap between pipes
+    self.gap_height = math.random( 1,9 ) *5 + 85
+    print(self.gap_height)
+
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + self.gap_height)
     }
 
     -- whether this pipe pair is ready to be removed from the scene
--- states/PlayState.lua
+++ states/PlayState.lua
@@ -22,17 +22,19 @@ function PlayState:init()
     self.pipePairs = {}
     self.timer = 0
     self.score = 0
+    self.spawn_time = math.random( 2, 4 )
 
     -- initialize our last recorded Y value for a gap placement to base other gaps off of
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
+    
 end
 
 function PlayState:update(dt)
     -- update timer for pipe spawning
     self.timer = self.timer + dt
-
+    
     -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
+    if self.timer >  self.spawn_time then
         -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
         -- no higher than 10 pixels below the top edge of the screen,
         -- and no lower than a gap length (90 pixels) from the bottom
